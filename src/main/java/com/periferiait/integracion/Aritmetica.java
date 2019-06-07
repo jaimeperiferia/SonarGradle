d9623aea34b3fb9063c44f26aac06d1fc7dac2b1
Jaime bonilla <jaimebonilla@cbit-online.com>
2019-06-07 14:18:38 -0500

    Verios con main

--- src/main/java/com/periferiait/integracion/Aritmetica.java
+++ src/main/java/com/periferiait/integracion/Aritmetica.java
@@ -10,14 +10,22 @@ package com.periferiait.integracion;
  * @author jaime
  */
 public class Aritmetica {
-    
-    public int suma(int a, int b){
-        return a+b;
+
+    static public int suma(int a, int b) {
+        return a + b;
     }
-    
-    
-        public int multiplicacion(int a, int b){
-        return a*b;
-        
+
+    static public int multiplicacion(int a, int b) {
+        return a * b;
+
     }
+
+    public static void main(String[] args) {
+
+        //Aquí las instrucciones de inicio y control del programa
+        System.out.println("Empezamos la ejecución del programa");
+        int a=1;
+        System.out.println(suma(a, a));
+
+    } //Cierre del main
 }
